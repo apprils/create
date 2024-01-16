5ec77ceea38257a5bf9de512e5d5ddbd96938c8c
Slee Woo <mail@sleewoo.com>
2024-01-16 12:33:33 +0200

    src/api/_server_watch

---
+++ lib/src/api/_server_watch.ts
@@ -0,0 +1,12 @@
+
+import { type Server } from "http";
+
+import "~/init";
+import { apiPort } from "@/package.json";
+
+export { app } from "./_app";
+
+export function listen(server: Server) {
+  server.listen(apiPort)
+}
+
