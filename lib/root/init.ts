3d25d151f832a8d7fc27115beeeedccf971f3ed2
Slee Woo <mail@sleewoo.com>
2024-03-07 19:01:08 +0200

    chore

--- lib/root/init.ts
+++ lib/root/init.ts
@@ -1,11 +1,11 @@
 // supposed to be used in CommonJS environment only
 
-process.on("uncaughtException", function (error) {
+process.on("uncaughtException", (error) => {
   console.error("UncaughtException", error);
   process.exit(1);
 });
 
-process.on("unhandledRejection", function (reason, promise) {
+process.on("unhandledRejection", (reason, promise) => {
   console.error("UnhandledRejection", promise, reason);
   process.exit(1);
 });
