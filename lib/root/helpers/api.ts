6f8142eeb625071f1515359e8a26f4c0375ed7cb
Slee Woo <mail@sleewoo.com>
2024-03-07 19:01:53 +0200

    chore

--- lib/root/helpers/api.ts
+++ lib/root/helpers/api.ts
@@ -1,4 +1,4 @@
-import type { Middleware } from "~/api/base";
+import type { Middleware } from "koa";
 
 export const errorHandler: Middleware = async function errorHandler(ctx, next) {
   try {
@@ -14,21 +14,23 @@ export const errorHandler: Middleware = async function errorHandler(ctx, next) {
   }
 };
 
-function extractCodeWithMessage(error: any): [number, string] {
+function extractCodeWithMessage(error: unknown): [number, string] {
   // allows throwing strings:
   //   throw "some error"
   //   throw "500: some error"
   if (typeof error === "string") {
-    // prettier-ignore
+    // biome-ignore format:
     const [
-      _ /** ignore */,
+      _, /** placeholder, ignore */
       code = 400,
       message = error,
     ] = error.trim().match(/^(\d+):\s*([^\0]+)/) || [];
     return [Number(code), message];
-  } else if (error.message) {
-    return [400, error.message];
-  } else {
-    return [500, "Unknown Error Occurred"];
   }
+
+  if (error instanceof Error) {
+    return [400, error.message];
+  }
+
+  return [500, "Unknown Error Occurred"];
 }
